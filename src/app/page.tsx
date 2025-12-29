520a8a97be26f5761d4b5093a2aef61df66da615
vikasprins <vikasprins15740@gmail.com>
2025-12-29 11:42:08 +0530

    change in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import AboutSection from "@/components/AboutSection";
-import CoursesSection from "@/components/CoursesSection";
+
 import Hero from "@/components/Hero";
 import InfoSection from "@/components/InfoSection";
-import Navbar from "@/components/Navbar";
+
 import TeamSection from "@/components/TeamSection";
 import Testimonial from "@/components/Testimonial";
 import WhyChooseSection from "@/components/WhyChooseSection";
@@ -11,12 +11,10 @@ import WhyChooseSection from "@/components/WhyChooseSection";
 export default function Home() {
   return (
     <>
-    {/* <Header /> */}
-    {/* <Navbar /> */}
     <Hero />
     <InfoSection />
     <AboutSection />
-    {/* <CoursesSection /> */}
+   
     <WhyChooseSection />
     <Testimonial />
     <TeamSection />
